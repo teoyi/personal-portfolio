8381488fccfc51d6755a712b3361d8f9194a130b
YiPeng Teo <yipengteo@yahoo.com.sg>
2021-12-11 22:10:00 +0800

    Attempt at horizontal timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -5,13 +5,47 @@ const Timeline = () => {
     return ( 
         <div className={classes.timeline} id='timeline'>
             <div className={classes.contentWrapper}>
-                <h3>My development journey</h3>
-                <div className={classes.milestone}>
+                <h3 className={classes.title}>My development journey</h3>
+                {/* <div className={classes.milestone}>
                     <p>Year 3 Physics undergraduate</p>
                     <p>Graduation</p>
                     <p>Junior Web Developer @ RPAT Pte Ltd</p>
                     <p>Seeking opportunities</p>
-                </div>
+                </div> */}
+                <section className={classes.milestone}>
+                    <ol>
+                        <li>
+                            <div>
+                                <time>July, 2020</time>
+                                Year 3 Physics Undergraduate, start of doing research full scale and python automation
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <time>August 2020</time>
+                                Began The Odin Project
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <time>June, 2021</time>
+                                Finalized research automation script, completed graduation thesis, and successfully graduated from Oregon State University
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <time>August, 2021</time>
+                                Junior Web Developer @ RPAT Pte Ltd
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <time>November, 2021</time>
+                                Software Engineer @ Craft Digital SG
+                            </div>
+                        </li>
+                    </ol>
+                </section>
                 <div>
                     <p>Skills I have acquired</p>
                     <div className={classes.skillsWrapper}>
