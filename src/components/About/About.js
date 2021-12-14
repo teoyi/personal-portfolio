31056533f2dfbd7a9ae6d21f3ec8536f8e622561
YiPeng Teo <yipengteo@yahoo.com.sg>
2021-12-14 16:00:56 +0800

    redid about component to be of mobile structuer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,11 +5,20 @@ import classes from '../../styles/About.module.css'
 const About = () => {
     return ( 
         <div className={classes.about} id="about">
-            <div className={classes.aboutContent}>
-                <h2>Hello there!</h2>
-                <p>My name is YiPeng and I go by the name Luke. I am a self-taught developer that has since been involved in projects in both personal and professional setting. Action speaks louder than words, so I will let this website speak on behalf my skills.</p>
+            <h2 className={classes.greetings}>Hello there!</h2>
+            <div className={classes.imageWrapper}>
+                <div className={classes.aboutImage}>
+                    <Image className={classes.image} src={profilePic} alt="me" />
+                </div>
             </div>
-            <Image className={classes.aboutImage} src={profilePic} alt="me" />
+            <div className={classes.subtitleWrapper}>
+                <p className={classes.subtitle}>
+                    My name is <span className={classes.firstName}>YiPeng</span> and I go by the name <span className={classes.engName}>Luke</span>. 
+                    <br />
+                    <br />
+                    I am a self-taught developer that has since been involved in projects in both personal and professional setting. Action speaks louder than words, so I will let this website speak on behalf my skills.
+                </p>
+            </div>
         </div>
         
     );
