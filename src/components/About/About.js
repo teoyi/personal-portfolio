0b29c5e269d3d1f6f01b979c523565201d6c9a39
YiPeng Teo <yipengteo@yahoo.com.sg>
2021-12-21 17:00:04 +0800

    header and landing is now responsive. Need to redo about page, focus more on coding aspects than school

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,23 +13,31 @@ const About = () => {
             <div className={classes.mainText}>
                 ABOUT
             </div>
-            <div className={classes.leftText}>
-                <div className={classes.leftTitle}><span className={classes.preText}>Graduate of</span><br /><span className={classes.title}>Oregon State University</span></div>
-                <div className={classes.para}>Majored in Physics and minored in finance</div>
-                <div className={classes.para}>Honors College Associate</div>
-                <div className={classes.para}>Author of a research automation script</div>
-                <div className={classes.para}>Successful completion of research thesis and thesis defence</div>
-                <div className={classes.para}>Graduated with the highest distinction, Summa Cum Laude</div>
+            <div className={classes.imgWrapper}>
+                <div className={classes.aboutImageMobile}>
+                    <Image src={profilePicCropped} alt="me" layout='fill' objectFit='contain'  />
+                </div>
             </div>
-            <div className={classes.aboutImage1}>
-                <Image src={profilePicMain} alt="me" layout='fill' objectFit='contain'  />
+            <div className={classes.contentWrapper}>
+                {/* <div className={classes.leftText}>
+                    <div className={classes.leftTitle}><span className={classes.preText}>Graduate of</span><br /><span className={classes.title}>Oregon State University</span></div>
+                    <div className={classes.para}>Majored in Physics and minored in finance</div>
+                    <div className={classes.para}>Honors College Associate</div>
+                    <div className={classes.para}>Author of a research automation script</div>
+                    <div className={classes.para}>Successful completion of research thesis and thesis defence</div>
+                    <div className={classes.para}>Graduated with the highest distinction, Summa Cum Laude</div>
+                </div> */}
+                <div className={classes.aboutImage1}>
+                    <Image src={profilePicMain} alt="me" layout='fill' objectFit='contain'  />
+                </div>
+                <div className={classes.rightText}>
+                    <div className={classes.title}>Self-taught Developer</div>
+                    <div className={classes.para}>Learnt web development through<br /> The Odin Project</div>
+                    <div className={classes.para}>Became a Junior Full-Stack web developer <br /> @ RPAT Pte Ltd</div>
+                    <div className={classes.para}>Currently a Software Engineer <br/>@ Craft Digital Pte Ltd</div>
+                </div>
             </div>
-            <div className={classes.rightText}>
-                <div className={classes.title}>Self-taught Developer</div>
-                <div className={classes.para}>Learnt web development through<br /> The Odin Project</div>
-                <div className={classes.para}>Became a Junior Full-Stack web developer <br /> @ RPAT Pte Ltd</div>
-                <div className={classes.para}>Currently a Software Engineer <br/>@ Craft Digital Pte Ltd</div>
-            </div>
+                
         </section>
         
     );
