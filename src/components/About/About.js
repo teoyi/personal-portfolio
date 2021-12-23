f9d8f7bf65b4c60806d96bb557010a9f9a0728bd
YiPeng Teo <yipengteo@yahoo.com.sg>
2021-12-23 21:12:32 +0800

    fixed bug on landing subtext, redid about section. About section requires ersponsive css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import profilePicCropped from '../../../public/static/images/theonly_square.png'
 import profilePicMain from '../../../public/static/images/theonly_main.png'
 import classes from '../../styles/About.module.css'
+import { SiJavascript, SiHtml5, SiCss3, SiBootstrap, SiReact, SiPython, SiMysql, SiPhp, SiNextdotjs, SiScikitlearn, SiNumpy, SiPandas  } from "react-icons/si";
 
 
 const About = () => {
@@ -19,25 +20,39 @@ const About = () => {
                 </div>
             </div>
             <div className={classes.contentWrapper}>
-                {/* <div className={classes.leftText}>
-                    <div className={classes.leftTitle}><span className={classes.preText}>Graduate of</span><br /><span className={classes.title}>Oregon State University</span></div>
-                    <div className={classes.para}>Majored in Physics and minored in finance</div>
-                    <div className={classes.para}>Honors College Associate</div>
-                    <div className={classes.para}>Author of a research automation script</div>
-                    <div className={classes.para}>Successful completion of research thesis and thesis defence</div>
-                    <div className={classes.para}>Graduated with the highest distinction, Summa Cum Laude</div>
-                </div> */}
                 <div className={classes.aboutImage1}>
                     <Image src={profilePicMain} alt="me" layout='fill' objectFit='contain'  />
                 </div>
-                <div className={classes.rightText}>
-                    <div className={classes.title}>Self-taught Developer</div>
-                    <div className={classes.para}>Learnt web development through<br /> The Odin Project</div>
-                    <div className={classes.para}>Became a Junior Full-Stack web developer <br /> @ RPAT Pte Ltd</div>
-                    <div className={classes.para}>Currently a Software Engineer <br/>@ Craft Digital Pte Ltd</div>
-                </div>
+                {/* <div className={classes.aboutContent}> */}
+                    <div className={classes.bottomText}>
+                        <div className={classes.title}>I&apos;m a self-taught developer, since then I have become familiar with the following</div>
+                        <ul className={classes.techExp}>
+                            <li>Web Development</li>
+                            <li>
+                                <div className={classes.icons}>
+                                    <SiHtml5 />
+                                    <SiCss3 />
+                                    <SiJavascript />
+                                    <SiBootstrap />
+                                    <SiReact />
+                                    <SiNextdotjs/>
+                                    <SiPhp />
+                                    <SiMysql />
+                                </div>
+                            </li>
+                            <li>Scientific Computing</li>
+                            <li>
+                                <div className={classes.icons}>
+                                    <SiPython />
+                                    <SiScikitlearn />
+                                    <SiNumpy />
+                                    <SiPandas />
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                {/* </div> */}
             </div>
-                
         </section>
         
     );
