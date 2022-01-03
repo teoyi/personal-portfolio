f9b405d330b2eeb6789f9c133b0e3788ee0e9c47
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-01-03 20:17:23 +0800

    test 3 of preview

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import thumbnail from "../../public/static/images/thumbnail.png"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1" />
-        <meta property="og:image" content="../../public/static/images/thumbnail.png" />
+        <meta property="og:image" content={thumbnail} />
       </Head>
       <Component {...pageProps} />
     </>
