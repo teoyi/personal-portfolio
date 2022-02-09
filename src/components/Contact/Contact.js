c20247318008e766ed7cd4a29c729751585418f7
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-02-09 23:11:32 +0800

    made slight css changes based on feedback

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,9 +8,17 @@ const Contact = () => {
         <div className={classes.contact} id="contact">
             <h1 className={classes.title}>Get in touch</h1>
             <p className={classes.contactContent}>
-                If you would like to get in touch with me about project collaboration or just to say hi, you can reach me at any of the social media platforms in the menu or send me an email directly at 
+                If you would like to get in touch with me about project collaboration or just to say hi, 
             </p>
+            <p className={classes.contactContent1}>reach out to me on any of the social media platforms </p>
+            <p className={classes.contactContent1}>or</p>
+            <p className={classes.contactContent1}>send me an email directly at </p>
             <div className={classes.email}><a href="mailto:yipengteo.luke@gmail.com">yipengteo.luke@gmail.com</a></div>
+            {/* <div className={classes.social}>
+                <a href="https://www.instagram.com/yipeng_/" className={classes.navSocials}><FaInstagramSquare size="4rem" /></a>
+                <a href="https://github.com/teoyi" className={classes.navSocials}><FaGithubSquare size="4rem" /></a>
+                <a href='https://www.linkedin.com/in/luketeo/' className={classes.navSocials}><FaLinkedin size="4rem" /></a>
+            </div> */}
         </div>
     );
 }
