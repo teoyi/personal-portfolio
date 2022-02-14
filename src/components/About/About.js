7d5acd53516cc0c4d65c0de50712b46fa7749051
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-02-14 15:41:05 +0800

    fixed responsivity except for contact page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,48 +8,51 @@ import { SiJavascript, SiHtml5, SiCss3, SiBootstrap, SiReact, SiPython, SiMysql,
 
 const About = () => {
     return ( 
-        <section className={classes.about} id="about">
-            {/* <div className={classes.vertLine}></div> */}
-            <div className={classes.mainText}>
-                ABOUT
-            </div>
-            {/* <div className={classes.imgWrapper}>
-                <div className={classes.aboutImageMobile}>
-                    <Image src={profilePicCropped} alt="me" layout='fill' objectFit='contain'  />
+        <section className={classes.wrapper} id="about">
+            <div className={classes.about}>
+                {/* <div className={classes.vertLine}></div> */}
+                <div className={classes.mainText}>
+                    ABOUT
                 </div>
-            </div> */}
-            <div className={classes.exteriorWrapper}>
-                <div className={classes.contentWrapper}>
-                    <div className={classes.aboutImage1}>
-                        <Image src={profilePicMain} alt="me" layout='fill' objectFit='contain'  />
-                    </div>
-                    <div className={classes.bottomText}>
-                        <div className={classes.preq}>
-                        I&apos;m a self-taught developer.
-                        </div>
-                        <div className={classes.skillsWrapper}>
-                            <div className={classes.title}>Acquired Skills</div>
-                            <hr className={classes.line} />
-                            <div className={classes.icons}> 
-                                <div className={classes.firstIcon}><SiJavascript /></div>
-                                <div className={classes.icon}><SiPhp /></div>
-                                <div className={classes.icon}><SiHtml5 /></div>
-                                <div className={classes.icon}><SiCss3 /></div>
-                                <div className={classes.icon}><SiTailwindcss /></div>
-                            </div>
-                            <div className={classes.icons}> 
-                                <div className={classes.firstIcon}><SiBootstrap /></div>
-                                <div className={classes.icon}><SiReact /></div>
-                                <div className={classes.icon}><SiNextdotjs /></div>
-                            </div>
-                            <div className={classes.icons}> 
-                                <div className={classes.firstIcon}><SiMongodb /></div>
-                                <div className={classes.icon}><SiPhpmyadmin /></div>
-                                <div className={classes.icon}><SiMysql /></div>
-                            </div>
-                        </div>
+                <div className={classes.imgWrapper}>
+                    <div className={classes.aboutImageMobile}>
+                        <Image src={profilePicCropped} alt="me" layout='fill' objectFit='contain'  />
                     </div>
                 </div>
+                <div className={classes.exteriorWrapper}>
+                    <div className={classes.contentWrapper}>
+                        <div className={classes.aboutImage1}>
+                            <Image src={profilePicMain} alt="me" layout='fill' objectFit='contain'  />
+                        </div>
+                        <div className={classes.bottomText}>
+                            <div className={classes.preq}>
+                            Self-taught developer and an avid learner of modern technologies.
+                            </div>
+                            <div className={classes.skillsWrapper}>
+                                <div className={classes.title}>Acquired Skills</div>
+                                <hr className={classes.line} />
+                                <div className={classes.icons}> 
+                                    <div className={classes.firstIcon}><SiJavascript /></div>
+                                    <div className={classes.icon}><SiPhp /></div>
+                                    <div className={classes.icon}><SiHtml5 /></div>
+                                    <div className={classes.icon}><SiCss3 /></div>
+                                    <div className={classes.icon}><SiTailwindcss /></div>
+                                </div>
+                                <div className={classes.icons}> 
+                                    <div className={classes.firstIcon}><SiBootstrap /></div>
+                                    <div className={classes.icon}><SiReact /></div>
+                                    <div className={classes.icon}><SiNextdotjs /></div>
+                                </div>
+                                <div className={classes.icons}> 
+                                    <div className={classes.firstIcon}><SiMongodb /></div>
+                                    <div className={classes.icon}><SiPhpmyadmin /></div>
+                                    <div className={classes.icon}><SiMysql /></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
         
