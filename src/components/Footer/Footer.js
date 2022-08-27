aaefb8a467bd21cb4ffce97ff3bc271505e59344
Luke Teo Yi Peng <yipengteo.luke@gmail.com>
2022-08-27 22:52:26 +0800

    Removed all past portfolio items, changing project to use tailwind

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import classes from '../../styles/Footer.module.css'
-
-const Footer = () => {
-    return (
-        <div className={classes.wrapper}>Copyright &copy; 2022, Luke Teo Yi Peng </div>
-    );
-}
- 
-export default Footer;
