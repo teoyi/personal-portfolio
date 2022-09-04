6527be4c87d86d9795eb073a6da550457fc1666d
Luke Teo <yipengteo.luke@gmail.com>
2022-09-04 15:14:46 +0800

    style: added responsive text sizes on name. Changed background to off black

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import Layout from "../common/layouts/standard";
 
 const Home: NextPage = () => {
     return (
-        <section className="h-screen w-full bg-black">
+        <section className="h-screen w-full bg-off-black">
             <Layout>
                 <div className="h-fscreen w-full flex justify-center items-center">
-                    <h1 className="text-8xl xl:text-16xl text-white font-zallord">
+                    <h1 className="text-8xl md:text-12xl xl:text-16xl text-white font-zallord">
                         LukeTeo
                     </h1>
                 </div>
