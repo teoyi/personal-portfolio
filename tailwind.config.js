cf5dbf39bfd9048b62864c2d11ec0b5e0f608dae
Luke Teo <yipengteo.luke@gmail.com>
2022-09-04 15:13:47 +0800

    style: added ne font sizes and added off-black colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,16 @@ module.exports = {
                 zallord: "Zallord",
             },
             fontSize: {
+                "12xl": "12rem",
                 "14xl": "14rem",
                 "16xl": "16rem",
             },
             height: {
                 nav: "80px",
                 fscreen: "calc(100vh - 80px)",
+            },
+            colors: {
+                "off-black": "#1E1C1C",
             },
         },
     },
