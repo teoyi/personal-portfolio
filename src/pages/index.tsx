ac5caf8e7fd538668b722c8186d7ba0648d24a5f
Luke Teo <yipengteo.luke@gmail.com>
2022-09-06 17:15:17 +0800

    feat: changed landing page background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../common/layouts/standard";
 
 const Home: NextPage = () => {
     return (
-        <section className="h-screen w-full bg-off-black">
+        <section className="h-screen w-full relative bg-landing bg-cover">
             <Layout>
                 <div className="h-fscreen w-full flex justify-center items-center">
                     <h1 className="text-8xl md:text-12xl xl:text-16xl text-white font-zallord">
